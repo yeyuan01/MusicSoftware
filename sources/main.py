19854c01652023f56feee6db1b4bcba6adf13fa7
yeyuan01 <413370960@qq.com>
2020-05-10 01:42:08 -0700

    try commit

--- sources/main.py
+++ sources/main.py
@@ -1,5 +1,9 @@
+# For self practice.
+# Some code learned from https://github.com/CharlesPikachu/Tools/blob/master/MusicPlayer/MusicPlayer.py written By @CharlesPikachu
 import sys
+import os
 from PyQt5.QtGui import *
+import time
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtMultimedia import *
@@ -11,8 +15,64 @@ class musicPlayer(QWidget, playerUI):
 	def __init__(self):
 		super(musicPlayer,self).__init__()
 		self.setupUi(self)
+		self.player = QMediaPlayer()
+		self.is_pause = True
+		self.is_switching = False
+		self.song_formats = ['mp3', 'm4a', 'flac', 'wav', 'ogg']
+		self.cur_song = ''
+		self.song_list = []
+		self.timer = QTimer(self)
+		self.timer.start(1000)
+		self.timer.timeout.connect(self.playByMode)
+		self.mode_btn.clicked.connect(self.changeMode)
+		self.horizontalSlider.sliderMoved[int].connect(lambda: self.player.setPosition(self.horizontalSlider.value()))
+		self.play_btn.clicked.connect(self.playMusic)
+		self.music_list.itemDoubleClicked.connect(self.doubleClickPlay)
+		self.cur_path = os.path.abspath(os.path.dirname(__file__))
+		self.select_folder.clicked.connect(self.openDir)
 		self.signin_btn.clicked.connect(self.showSignIn)
-		self.play_btn.clicked.connect(self.play)
+
+	def doubleClickPlay(self):
+		self.horizontalSlider.setValue(0)
+		self.is_switching = True
+		self.setCurrentSong()
+		self.playMusic()
+		self.is_switching = False
+	def changeMode(self):
+		if self.mode_btn.text() == 'All Repeat':
+			self.mode_btn.setText('Order')
+		elif self.mode_btn.text() == 'Order':
+			self.mode_btn.setText('Shuffle')
+		elif self.mode_btn.text() == 'Shuffle':
+			self.mode_btn.setText('Repeat Once')
+		elif self.mode_btn.text() == 'Repeat Once':
+			self.mode_btn.setText('All Repeat')
+	def playByMode(self):
+		if (not self.is_pause) and (not self.is_switching):
+			self.horizontalSlider.setMinimum(0)
+			self.horizontalSlider.setMaximum(self.player.duration())
+			self.horizontalSlider.setValue(self.horizontalSlider.value() + 1000)
+		self.label.setText(time.strftime('%M:%S', time.localtime(self.player.position() / 1000)))
+		self.label_2.setText(time.strftime('%M:%S', time.localtime(self.player.duration() / 1000)))
+		if self.mode_btn.text() == 'All Repeat' and (not self.is_pause) and (not self.is_switching):
+			if self.music_list == 0:
+				return
+			if self.player.position() == self.player.duration():
+				self.nextMusic()
+	def nextMusic(self):
+		self.horizontalSlider.setValue(0)
+		if self.music_list.count() == 0:
+			QMessageBox.warning(self, '', 'No music in list', QMessageBox.Ok)
+			return
+		if self.music_list.currentRow() == self.music_list.count():
+			self.music_list.setCurrentRow(0)
+		else:
+			self.music_list.setCurrentRow(self.music_list.currentRow() + 1)
+		self.is_switching = True
+		self.setCurrentSong()
+		self.playMusic()
+		self.is_switching = False
+
 	def showSignIn(self):
 		if self.signin_btn.text() == 'Sign in':
 			sign_in.show()
@@ -20,6 +80,45 @@ class musicPlayer(QWidget, playerUI):
 			self.signin_btn.setText('Sign in')
 			global user
 			user = ''
+	def openDir(self):
+		self.cur_path = QFileDialog.getExistingDirectory(self,'select folder',self.cur_path)
+		if self.cur_path:
+			self.music_list.clear()
+			for song in os.listdir(self.cur_path):
+				if song.split('.')[-1] in self.song_formats:
+					self.song_list.append([song, os.path.join(self.cur_path, song).replace('\\', '/')])
+					self.music_list.addItem(song)
+			self.music_list.setCurrentRow(0)
+			if self.song_list:
+				self.cur_song = self.song_list[self.music_list.currentRow()][-1]
+			self.setCurrentSong()
+			self.label.setText('00:00')
+			self.label_2.setText('00:00')
+			self.horizontalSlider.setSliderPosition(0)
+			self.is_pause = True
+			self.play_btn.setText('Play')
+	def playMusic(self):
+		if self.music_list.count() == 0:
+			QMessageBox.warning(self,'','No music in list', QMessageBox.Ok)
+			return
+		if not self.player.isAudioAvailable():
+			self.setCurrentSong()
+		if self.is_pause or self.is_switching:
+
+			self.player.play()
+			self.is_pause = False
+			self.play_btn.setText('Pause')
+			print("played")
+		elif (not self.is_pause) and (not self.is_switching):
+
+			self.player.pause()
+			self.is_pause = True
+			self.play_btn.setText('Play')
+			print("paused")
+	def setCurrentSong(self):
+		self.cur_song = self.song_list[self.music_list.currentRow()][-1]
+		self.CurrentSongName.setText(self.cur_song.split('/')[-1])
+		self.player.setMedia(QMediaContent(QUrl(self.cur_song)))
 	def play(self):
 		pass
 class signIn(QWidget, signinUI):
